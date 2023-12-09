b367690d7a5b9955bc2bd1451bfdb9815ef4b504
mclean1 <mclean1@cs.dal.ca>
2023-12-09 14:07:18 -0400

    Added error handler and fixed path for api

--- src/backend/server.js
+++ src/backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv').config();
 const colors = require('colors');
 const app = express();
 const connectDB = require('./config/db');
+const { errorHandler } = require('./middleware/errorMiddleware');
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -10,7 +11,9 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.use('api/games771', require('./routes/gameRoutes'));
+app.use('/api/games771', require('./routes/gameRoutes'));
+
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
